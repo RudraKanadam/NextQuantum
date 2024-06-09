217a08cf96a114da606adc4f884ee3e3c72e5e67
Rudra Kanadam <163636444+RudraKanadam@users.noreply.github.com>
2024-06-10 03:00:33 +0530

    Initial Vanilla NextJS

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/providers/theme-provider";
+import ModalProvider from "@/providers/modal-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AsyInk",
+  description: "Automate.Integrate.Accelerate",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ModalProvider>
+            {children}
+            <Toaster />
+          </ModalProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
