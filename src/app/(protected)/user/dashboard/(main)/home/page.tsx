30f73e0909e989f80a87cc679103277e560306d7
Rudra Kanadam <163636444+RudraKanadam@users.noreply.github.com>
2024-06-14 03:05:36 +0530

    adding some UI changes

--- src/app/(protected)/user/dashboard/(main)/home/page.tsx
+++ src/app/(protected)/user/dashboard/(main)/home/page.tsx
@@ -13,61 +13,60 @@ const DashboardPage = async () => {
 
   return (
     <div className="flex flex-col gap-4 relative p-4 md:p-6 lg:p-8">
-      <h1 className="text-4xl sticky top-0 z-[10] bg-background/50 backdrop-blur-lg flex items-center border-b p-4 md:p-6 lg:p-8">
+      <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
         Dashboard
       </h1>
-      <h1 className="text-3xl font-bold mb-6">
-        Welcome, {session?.user?.name}!
-      </h1>
-      {featureCardsEnabled && (
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-6">
-          {/* Feature Cards */}
-          <NextJsCard />
-          <AuthJsCard />
-          <ShadCnCard />
-          <AcernityCard />
-        </div>
-      )}
 
-      <div className="flex flex-col lg:flex-row gap-6 mt-6 mx-6">
-        {/* Accordion for Boilerplate FAQ */}
-        <div className="flex-1 lg:w-1/4">
-          <BoilerplateAccordion />
-        </div>
+      <main className="flex flex-col gap-6">
+        <section className="text-3xl font-bold mb-6">
+          Welcome, {session?.user?.name}!
+        </section>
 
-        {/* Notifications, Textarea and MeteorsCard */}
-        <div className="flex flex-1 flex-col lg:flex-row gap-6 mx-20">
-          <div className="flex-1 mx-28">
+        {featureCardsEnabled && (
+          <section className="grid grid-cols-1 md:grid-cols-4 gap-6">
+            {/* Feature Cards */}
+            <NextJsCard />
+            <AuthJsCard />
+            <ShadCnCard />
+            <AcernityCard />
+          </section>
+        )}
+
+        <section className="flex justify-center items-center flex-grow mt-6">
+          {/* Accordion for Boilerplate FAQ */}
+          <div className="w-full lg:w-1/2">
+            <BoilerplateAccordion />
+          </div>
+        </section>
+
+        <section className="flex justify-center items-center flex-grow mt-16">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 w-full lg:w-1/3 justify-center items-center">
+            <MeteorsCard />
+            <MeteorsCard />
+            <MeteorsCard />
             <MeteorsCard />
           </div>
-          <div className="flex-1">
+        </section>
+
+        <section className="flex justify-end items-center flex-grow mt-16">
+          <div className="flex justify-end items-center w-full lg:w-2/3">
             <Notifications />
           </div>
-          {/* <div className="flex-1">
-            <div className="grid w-full gap-1.5">
-              <Label htmlFor="message">Your message</Label>
-              <Textarea
-                placeholder="Type your message here."
-                id="message"
-                className="w-full"
-              />
-            </div>
-          </div> */}
-        </div>
-      </div>
+        </section>
 
-      {/* Quick Links */}
-      <div className="flex-1 bg-white dark:bg-black p-4 rounded-lg shadow-md mt-6">
-        <h2 className="text-xl font-semibold mb-2">Quick Links</h2>
-        <div className="flex gap-4">
-          <a href="/faq" className="text-blue-500 hover:underline">
-            FAQ
-          </a>
-          <a href="/resources" className="text-blue-500 hover:underline">
-            Resources
-          </a>
-        </div>
-      </div>
+        {/* Quick Links */}
+        <section className="flex-1 bg-white dark:bg-gray-900 p-4 rounded-lg shadow-md mt-6">
+          <h2 className="text-xl font-semibold mb-2">Quick Links</h2>
+          <div className="flex gap-4">
+            <a href="/faq" className="text-blue-500 hover:underline">
+              FAQ
+            </a>
+            <a href="/resources" className="text-blue-500 hover:underline">
+              Resources
+            </a>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
+import React from "react";
+import { SparklesCore } from "@/components/ui/sparkles";
+import { LampEffect } from "@/components/lampEffect";
 import GlobalNavBar from "@/components/global/navbar";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center flex-col">
       <GlobalNavBar />
-
-      <div className="overflow-hidden bg-transparent w-full">Hello</div>
+      <div className="h-screen relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+        <div className="w-full absolute inset-0 h-full">
+          <SparklesCore
+            id="tsparticlesfullpage"
+            background="transparent"
+            minSize={0.6}
+            maxSize={1.4}
+            particleDensity={100}
+            className="w-full h-full"
+            particleColor="#FFFFFF"
+          />
+        </div>
+        <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20">
+          Boilerplate - NextQuantum
+        </h1>
+      </div>
+      <LampEffect />
     </main>
   );
 }
